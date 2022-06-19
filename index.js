121e249eb520d276fe553a225f24cb5f5dcc83ea
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2022-06-19 15:18:01 +0300

    Exercises 3.5 and 3.6 completed

--- index.js
+++ index.js
@@ -52,6 +52,38 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'Number missing'
+    })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'Name must be unique'
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 100000),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
