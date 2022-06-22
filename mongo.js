04f60ec89f602c596478348963af2760f5768d47
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2022-06-22 19:30:09 +0300

    Exercise 3.12 completed
    Command-line database created

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.hyxmreu.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+
+if (process.argv.length === 5) {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+      })
+
+      return person.save()
+    })
+    .then(() => {
+      console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+      return mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+}
+
+if (process.argv.length === 3) {
+  console.log('phonebook')
+  mongoose
+    .connect(url)
+    Person.find({}).then(result => {
+      result.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+} 
